93409c90430885490de589649bec91c98df84ae8
julio mol <juliomolina17@gmail.com>
2022-11-09 03:21:24 -0300

    first commit

---
+++ ProyectoFinal/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Models;
+using ProyectoFinal.Rules;
+using System.Diagnostics;
+
+namespace ProyectoFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Post(int id)
+        {
+            var rule = new PublicacionRule(_configuration);
+            var post = rule.GetPostById(id);
+            if (post==null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        public IActionResult Index()
+        {
+            var rule = new PublicacionRule(_configuration);
+            var posts = rule.GetPostToHome();
+            return View(posts);
+        }
+
+        public IActionResult Suerte()
+        {
+            //le paso la config al constructor de PublicacionRule
+            var rule = new PublicacionRule(_configuration);
+            var post = rule.GetOnePostRandom();
+            return View(post);
+        }
+
+        public IActionResult AcercaDe()
+        {
+            return View();
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProyectoFinal/Rules/PublicacionRule.cs
@@ -0,0 +1,62 @@
+using Dapper;
+using ProyectoFinal.Models;
+using System.Data.SqlClient;
+
+namespace ProyectoFinal.Rules
+{
+    public class PublicacionRule
+    {
+        private readonly IConfiguration _configuration;
+        public PublicacionRule(IConfiguration configuration)
+        {
+           _configuration = configuration;
+        }
+
+        public Publicacion GetOnePostRandom()
+        {
+            var connectionString = _configuration.GetConnectionString("BlogDatabase");
+            //var connectionString = @"Server= .\SQLEXPRESS; Database = BlogDatabase; Trusted_Connection=true ";
+            using var connection = new SqlConnection(connectionString);
+            {
+                connection.Open();
+                var post = connection.Query<Publicacion>("SELECT TOP 1 * FROM Publicacion");
+                return post.First();
+            }
+            /*var post = new Publicacion
+            {
+                Titulo = "Man must explore, and this is exploration at its greatest",
+                SubTitulo = "Problems look mighty small from 150 miles up",
+                Cuerpo = "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm",
+                Creacion = new DateTime(2022, 11, 01),
+                Creador = "Jul Mol",
+                Imagen = "/assets/img/post-bg.jpg"
+            };
+            return post;*/
+        }
+
+        public List<Publicacion> GetPostToHome()
+        {
+            var connectionString = _configuration.GetConnectionString("BlogDatabase");
+            //var connectionString = @"Server= .\SQLEXPRESS; Database = BlogDatabase; Trusted_Connection=true ";
+            using var connection = new SqlConnection(connectionString);
+            {
+                connection.Open();
+                var post = connection.Query<Publicacion>("SELECT TOP 4 * FROM Publicacion ORDER BY creacion DESC");
+                return post.ToList();
+            }
+        }
+
+        public Publicacion GetPostById(int id)
+        {
+            var connectionString = _configuration.GetConnectionString("BlogDatabase");
+            //var connectionString = @"Server= .\SQLEXPRESS; Database = BlogDatabase; Trusted_Connection=true ";
+            using var connection = new SqlConnection(connectionString);
+            {
+                connection.Open();
+                var query = "SELECT * FROM Publicacion WHERE Id=@id";
+                var post = connection.QueryFirstOrDefault<Publicacion>(query, new {id=id});
+                return post;
+            }
+        }
+    }
+}
